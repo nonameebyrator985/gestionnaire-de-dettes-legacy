83c3fe11336b7c90ee899480ca2483e42a86fe6a
nonameebyrator985 <singheingemo@hotmail.com>
2025-08-31 23:55:52 +0900

    feat: initial commit for src/config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: 'localhost',
+        user: 'username',
+        password: 'password',
+        database: 'debts_db'
+    }
+};
+
+module.exports = config;
